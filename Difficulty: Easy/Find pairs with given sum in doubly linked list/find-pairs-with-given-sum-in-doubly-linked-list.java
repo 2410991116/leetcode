c5088ee1cdfe1bc9026735cd8fa9248171605be2
Jasnoor Singh <jasnoortoor1984@gmail.com>
2025-12-04 12:04:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find pairs with given sum in doubly linked list/find-pairs-with-given-sum-in-doubly-linked-list.java
@@ -0,0 +1,37 @@
+class Solution {
+    public static ArrayList<ArrayList<Integer>> findPairsWithGivenSum(int target,Node head) {
+        
+        ArrayList<ArrayList<Integer>> result=new ArrayList<>();
+        
+        if (head==null || head.next==null){
+            return result;
+        }
+        
+        Node left=head;
+        Node right=head;
+        
+        while (right.next!=null){
+            right=right.next;
+        }
+        
+        while (left!=null && right!=null && left.data<right.data){
+            int sum=left.data+right.data;
+            
+            if (sum==target){
+                ArrayList<Integer> pair=new ArrayList<>();
+                pair.add(left.data);
+                pair.add(right.data);
+                result.add(pair);
+                
+                left=left.next;
+                right=right.prev;
+            }else if(sum<target){
+                left=left.next;
+            }else{
+                right=right.prev;
+            }
+        }
+        
+        return result;
+    }
+}
