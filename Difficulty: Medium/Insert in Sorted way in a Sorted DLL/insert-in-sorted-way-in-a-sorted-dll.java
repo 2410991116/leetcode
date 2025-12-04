0cd859dbd7f31b2e87f879440ae4182db43bcdbd
Jasnoor Singh <jasnoortoor1984@gmail.com>
2025-12-04 11:34:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Insert in Sorted way in a Sorted DLL/insert-in-sorted-way-in-a-sorted-dll.java
+++ Difficulty: Medium/Insert in Sorted way in a Sorted DLL/insert-in-sorted-way-in-a-sorted-dll.java
@@ -1,49 +1,33 @@
-/*class of the node of the DLL is as
-/*
-class Node {
-    int data;
-    Node prev, next;
-    Node(int data) {
-        this.data = data;
-        this.prev = this.next = null;
-    }
-}
-*/
 class Solution {
     public Node sortedInsert(Node head, int x) {
         // add your code here
-        Node newnode=new Node(x);
+        Node result=new Node(x);
         
         if (head==null){
-            return newnode;
+            return result;
         }
         
-        if (x<=head.data){
-            newnode.next=head;
-            head.prev=newnode;
-            return newnode;
-        }
-        
-        Node dummy=head;
-        
-        while (dummy.next!=null && dummy.data<x){
-            dummy=dummy.next;
-        }
-        
-        if (dummy.next==null && dummy.data<x){
-            dummy.next=newnode;
-            newnode.prev=dummy;
+        if (head.data>x){
+            result.next=head;
+            head.prev=result;
+            head=result;
             return head;
         }
         
-        Node temp=dummy.prev;
-        temp.next=newnode;
-        newnode.prev=temp;
+        Node dummy=head;
+        while (dummy.next!=null && dummy.next.data<x){
+            dummy=dummy.next;
+        }
         
-        newnode.next=dummy;
-        dummy.prev=newnode;
+        result.next=dummy.next;
+        result.prev=dummy;
+        
+        if (dummy.next!=null){
+            dummy.next.prev=result;
+        }
+        
+        dummy.next=result;
         
         return head;
-        
     }
 }
