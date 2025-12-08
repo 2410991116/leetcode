c7c4e668d32ed7144247c3b62c2ee48b9d6e3888
Jasnoor Singh <jasnoortoor1984@gmail.com>
2025-12-08 12:33:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Stock span problem/stock-span-problem.java
+++ Difficulty: Medium/Stock span problem/stock-span-problem.java
@@ -1,32 +1,24 @@
 class Solution {
     public ArrayList<Integer> calculateSpan(int[] arr) {
         // code here
-        ArrayList<Integer> span = new ArrayList<>();
-        Stack<Integer> st = new Stack<>();   // store indexes
-
-        // First day span is always 1
-        st.push(0);
-        span.add(1);
-
-        for (int i = 1; i < arr.length; i++) {
-
-            // Remove all smaller prices
-            while (!st.isEmpty() && arr[i] >= arr[st.peek()]) {
+        ArrayList<Integer> nums=new ArrayList<>();
+        
+        Stack<Integer> st=new Stack<>();
+        
+        for (int i=0;i<arr.length;i++){
+            while (!st.isEmpty() && arr[st.peek()]<=arr[i]){
                 st.pop();
             }
-
-            // If stack empty => all previous are smaller
-            if (st.isEmpty()) {
-                span.add(i + 1);
-            } else {
-                // span = distance to previous greater price
-                span.add(i - st.peek());
+            
+            if (st.isEmpty()){
+                nums.add(i+1);
+            }else{
+                nums.add(i-st.peek());
             }
-
-            // Add current day index
+            
             st.push(i);
         }
-
-        return span;
+        
+        return nums;
     }
 }
