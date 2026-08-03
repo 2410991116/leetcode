67ec9b818eb6444ead5aaf64b98b2722c15cb00e
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-08-03 12:33:30 +0530

    Time: 1 ms (59.16%), Space: 47.4 MB (96.14%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,22 +1,23 @@
 class Solution {
-    public int trap(int[] nums) {
+    public int trap(int[] arr) {
         int sum=0;
         int lmax=Integer.MIN_VALUE;
         int rmax=Integer.MIN_VALUE;
 
         int left=0;
-        int right=nums.length-1;
+        int right=arr.length-1;
+
         while (left<right){
-            lmax=Math.max(lmax,nums[left]);
-            rmax=Math.max(rmax,nums[right]);
+            lmax=Math.max(lmax,arr[left]);
+            rmax=Math.max(rmax,arr[right]);
 
             if (lmax<rmax){
-                sum+=lmax-nums[left];
+                sum+=lmax-arr[left];
                 left++;
             }else{
-                sum+=rmax-nums[right];
+                sum+=rmax-arr[right];
                 right--;
-            }
+            }           
         }
 
         return sum;
