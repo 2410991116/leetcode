d97057149fe32861a1dda94094f5f32138e452ed
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-08-05 12:07:46 +0530

    Time: 9 ms (11.07%), Space: 47.3 MB (16.25%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.java
@@ -1,27 +1,12 @@
 class Solution {
-    public boolean sorted(String s){
-        for (int i=1;i<s.length();i++){
-            if (s.charAt(i)<s.charAt(i-1)){
-                return false;
-            }
-        }
-
-        return true;
-    }
     public int minDeletionSize(String[] str) {
-        ArrayList<String> arr=new ArrayList<>();
-        for (int i=0;i<str[0].length();i++){
-            StringBuilder sb=new StringBuilder();
-            for (int j=0;j<str.length;j++){
-                sb.append(str[j].charAt(i));
-            }
-            arr.add(sb.toString());
-        }
-
         int count=0;
-        for (int i=0;i<arr.size();i++){
-            if (!sorted(arr.get(i))){
-                count++;
+        for (int i=0;i<str[0].length();i++){
+            for (int j=1;j<str.length;j++){
+                if (str[j].charAt(i)<str[j-1].charAt(i)){
+                    count++;
+                    break;
+                }
             }
         }
 
