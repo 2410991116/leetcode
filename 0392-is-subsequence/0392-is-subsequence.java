deda962c7975d2cb823389c7f19bf9e9e98d975a
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-08-07 13:13:49 +0530

    Time: 5 ms (1.81%), Space: 43.7 MB (11.33%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,19 +1,52 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int i=0;
-        int j=0;
-
-        while (i<s.length() && j<t.length()){
-            if (s.charAt(i)==t.charAt(j)){
-                i++;
+        HashMap<Character,List<Integer>> map=new HashMap<>();
+        for (int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            if (map.containsKey(ch)){
+                List<Integer> l=map.get(ch);
+                l.add(i);
+                map.put(ch,l);
+            }else{
+                List<Integer> l=new ArrayList<>();
+                l.add(i);
+                map.put(ch,l);
             }
-            j++;
         }
 
-        if (i==s.length()){
-            return true;
-        }else{
-            return false;
+        int prev=-1;
+        for (char ch:s.toCharArray()){
+            if (!map.containsKey(ch)){
+                return false;
+            }
+
+            List<Integer> l=map.get(ch);
+            int idx=upperBound(l,prev);
+
+            if (idx==l.size()){
+                return false;
+            }
+
+            prev=l.get(idx);
         }
+
+        return true;
+    }
+
+    public int upperBound(List<Integer> l,int prev){
+        int low = 0;
+        int high = l.size();
+
+        while (low < high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (l.get(mid) <= prev)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+
+        return low;
     }
 }
