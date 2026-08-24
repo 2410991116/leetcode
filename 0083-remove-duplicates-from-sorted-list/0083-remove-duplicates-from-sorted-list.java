805c2e48a9ca64eecc632d6bf488ad3fc7a3c2cf
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-08-24 10:30:29 +0530

    Time: 0 ms (100%), Space: 46.2 MB (5.44%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -1,38 +1,23 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        ArrayList<Integer> arr=new ArrayList<>();
-        ListNode temp=head;
+        if (head==null){
+            return head;
+        }
+        
+        ListNode answer=new ListNode(head.val);
+        ListNode ans=answer;
+
+        ListNode temp=head.next;
+        int prev=head.val;
         while (temp!=null){
-            arr.add(temp.val);
+            if (temp.val!=prev){
+                ans.next=new ListNode(temp.val);
+                prev=temp.val;
+                ans=ans.next;
+            }
             temp=temp.next;
         }
 
-        ArrayList<Integer> nums=new ArrayList<>();
-        HashSet<Integer> map=new HashSet<>();
-        for (int i=0;i<arr.size();i++){
-            if (!map.contains(arr.get(i))){
-                map.add(arr.get(i));
-                nums.add(arr.get(i));
-            }
-        }
-
-        ListNode result=new ListNode(0);
-        ListNode res=result;
-        for (int i=0;i<nums.size();i++){
-            res.next=new ListNode(nums.get(i));
-            res=res.next;
-        }
-
-        return result.next;
+        return answer;
     }
 }
