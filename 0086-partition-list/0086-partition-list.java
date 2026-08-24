c06e4c4349dbf2be294efd46b14bb8ba17a6e9e9
Jasnoor Singh <jasnoortoor1984@gmail.com>
2026-08-24 11:47:54 +0530

    Time: 0 ms (100%), Space: 43.6 MB (84.4%) - LeetHub

---
+++ 0086-partition-list/0086-partition-list.java
@@ -0,0 +1,29 @@
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode smaller=new ListNode(0);
+        ListNode small=smaller;
+
+        ListNode larger=new ListNode(0);
+        ListNode large=larger;
+
+        ListNode temp=head;
+
+        while (temp!=null){
+            if (temp.val < x){
+                small.next=new ListNode(temp.val);
+                small=small.next;
+            }else{
+                large.next=new ListNode(temp.val);
+                large=large.next;
+            }
+            temp=temp.next;
+        }
+
+        larger=larger.next;
+        large.next=null;
+        small.next=larger;
+
+        return smaller.next;       
+
+    }
+}
